1de8094341d69cbf23c1fede0fa85907f72824d1
parkjaehoon <yukihoon12@naver.com>
2021-03-21 23:26:38 +0900

    front setting

---
+++ front/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.min.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        loader: "ts-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.(jpg|png|jpeg|bmp|gif|svg)?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.css?$/,
+        loader: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      scriptLoading: "defer",
+      inject: "head",
+      cache: true
+    }),
+    new Dotenv({
+      path: path.join(__dirname, "src/.env")
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    inline: true,
+    hot: true
+  }
+};
